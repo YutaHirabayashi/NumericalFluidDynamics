a250115045648eb6bd7f906d26845ae72ca23b2d
hiraba <hiraba@MacBookPro.local>
2019-12-31 16:39:02 +0900

    initial commit

---
+++ MacMethod.py
@@ -0,0 +1,13 @@
+import numpy as np
+def simulate_cavity_by_mac_method():
+    #計算の設定をする(ステップ数、ポアソン方程式の最大反復回数、Δt、Re)
+    
+    #流速の初期条件を設定する
+    #圧力の初期条件を設定する
+
+    #for
+
+        #圧力のポアソン方程式を解く
+
+        #流速を更新する
+    return
---
+++ main.py
@@ -0,0 +1,11 @@
+from MacMethod import simulate_cavity_by_mac_method
+
+
+def main():
+    simulate_cavity_by_mac_method()
+    return
+
+
+if __name__ == "__main__":
+    main()
+    pass
